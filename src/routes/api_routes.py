0dbfc4db12a385b4891d62cce40dc4c5e64f6d89
Mikael Moreira <160345147+KwoonXD@users.noreply.github.com>
2026-01-06 10:55:31 -0300

    .

--- src/routes/api_routes.py
+++ src/routes/api_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, url_for
 from flask_login import login_required
 from ..services.chamado_service import ChamadoService
 from ..models import Cliente, Chamado, Tecnico, db, TecnicoStock, ItemLPU, Pagamento
